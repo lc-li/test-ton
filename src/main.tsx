68aad0b0f5272fc66ac871eb1a96826159d5709c
lc <lc@lcdeMacBook-Air.local>
2024-11-02 16:48:31 +0800

    修改manifest文件

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
-const manifestUrl = "https://lc-li.github.io/test-ton/tonconnect-mainfest.json"
+const manifestUrl = "https://lc-li.github.io/test-ton/tonconnect-manifest.json"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
